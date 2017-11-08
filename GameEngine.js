d23b7856668b2221249714ebe039f0bfc891444d
blueridger <nath.sears@gmail.com>
2017-11-07 18:28:16 -0700

    Async design

--- GameEngine.js
+++ GameEngine.js
@@ -1,8 +1,15 @@
+//import GUI and map gen
 
 function GameEngine() {
   //Start Properties
   
+  this.callback = function{};
+  
+  var debug = true;
   var map = null;
+  var interval;
+  var interpreter = null;
+  var play = false;
   
   //End Properties
   //Start Main
@@ -10,26 +17,31 @@ function GameEngine() {
   //End Main
   //Start Private Methods
   
+  function setupInterpreter() {
+    
+  }
+  
   function moveForward() {
   
   }
   
   //End Private Methods
   //Start Privileged Methods
+
+  //return boolean true if level won
+  this.step = function() {
+    setupInterpreter();
+  }
   
   //return boolean true if level won
   this.play = function() {
-
+    callback = this.step;
+    this.step();
   }
 
   //return void
   this.pause = function() {
-
-  }
-
-  //return boolean true if level won
-  this.step = function() {
-
+    callback = function{};
   }
 
   //return void
