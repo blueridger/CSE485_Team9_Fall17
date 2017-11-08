4479a6475de1f4336beae4c5f6498fee0a5e9017
blueridger <nath.sears@gmail.com>
2017-11-07 17:01:01 -0700

    GameEngine skeleton

---
+++ GameEngine.js
@@ -0,0 +1,40 @@
+
+function GameEngine() {
+  //Start Properties
+  
+  var map = null;
+  
+  //End Properties
+  //Start Main
+  
+  //End Main
+  //Start Private Methods
+  
+  function moveForward() {
+  
+  }
+  
+  //End Private Methods
+  //Start Privileged Methods
+  
+  //return boolean true if level won
+  this.play = function() {
+
+  }
+
+  //return void
+  this.pause = function() {
+
+  }
+
+  //return boolean true if level won
+  this.step = function() {
+
+  }
+
+  //return void
+  this.resetLevel = function() {
+
+  }
+  //End Privileged Methods
+}
