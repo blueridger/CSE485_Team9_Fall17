ee8f54b77b93c20e80559ce797a9de2f345b649d
blueridger <nath.sears@gmail.com>
2017-11-09 12:07:57 -0700

    play callback flag
    because JS is always pass-by-value

--- GameEngine.js
+++ GameEngine.js
@@ -3,12 +3,11 @@
 function GameEngine() {
   //Start Properties
   
-  this.callback = function{};
-  
   var debug = true;
   var map = null;
   var gui = null;
   var interpreter = null;
+  var play = false; //flag for play/pause functionality
   
   //End Properties
   //Start Main
@@ -21,26 +20,33 @@ function GameEngine() {
   }
   
   function moveForward() {
-  
+    
   }
   
   //End Private Methods
   //Start Privileged Methods
+  
+  this.callback = function{
+    if (play) this.step();
+  }
 
   //return boolean true if level won
   this.step = function() {
     setupInterpreter();
+    if (!interpreter.step()) {
+      //restart interpreter from the beginning
+    }
   }
   
   //return boolean true if level won
   this.play = function() {
-    callback = this.step;
+    play = true;
     this.step();
   }
 
   //return void
   this.pause = function() {
-    callback = function{};
+    play = false;
   }
 
   //return void
