799eb8c6d7c2fabf428588dae544625fcafc767f
wwnjj13 <wwnjj13@gmail.com>
2017-11-22 23:08:07 -0700

    Added JSInterpreter Code
    I added the code to step through the instructions while highlighting blocks. I also added the necessary html in the backendTest to load a Blockly workspace.

--- GameEngine.js
+++ GameEngine.js
@@ -12,7 +12,7 @@ function GameEngine(Debug) {
   
   //End Properties
   //Start Main
-  map = mapGenSTUB();
+  //map = mapGenSTUB();
   debug("Map Generated");
   gui = new guiMOCK(this);
   debug("GUI object created");
@@ -52,15 +52,111 @@ function GameEngine(Debug) {
   }
 
   function debug(message) {
-    if (debugBool) Console.log(message);
+    if (this.debugBool) Console.log(message);
   }
 
   function setupInterpreter() {
-    if (interpreter == null) {
-
-    } else {
-
+    if (interpreter == null) 
+    {
+      
+    } 
+    else 
+    {
+      //do nothing???
     }
+  }
+  
+  this.generateCodeAndLoadIntoInterpreter = function() 
+  {
+    // Generate JavaScript code and parse it.
+    Blockly.JavaScript.STATEMENT_PREFIX = 'highlightBlock(%1);\n';
+    Blockly.JavaScript.addReservedWords('highlightBlock');
+    latestCode = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+    resetStepUi(true);
+    debug("new code: " + latestCode);
+  }
+  
+  function resetStepUi(clearOutput) 
+  {
+    demoWorkspace.highlightBlock(null);
+    highlightPause = false;
+  }	
+  
+  //API to link the interpretter sandbox to the actual code
+  function initApi(interpreter, scope)
+  {
+    var wrapper;
+    wrapper = function(id) 
+    {
+      moveForward();
+    };
+    interpreter.setProperty(scope, 'moveForward',
+    interpreter.createNativeFunction(wrapper));
+		
+    wrapper = function(id) 
+    {
+      moveBackward();
+    };
+    interpreter.setProperty(scope, 'moveBackward',
+    interpreter.createNativeFunction(wrapper));
+		
+    wrapper = function(id) 
+    {
+      turnRight();
+    };
+    interpreter.setProperty(scope, 'turnRight',
+    interpreter.createNativeFunction(wrapper));
+		
+    wrapper = function(id) 
+    {
+      turnLeft();
+    };
+    interpreter.setProperty(scope, 'turnLeft',
+    interpreter.createNativeFunction(wrapper));
+		
+    wrapper = function(id) 
+    {
+      openRight();
+    };
+    interpreter.setProperty(scope, 'openRight',
+    interpreter.createNativeFunction(wrapper));
+		
+    wrapper = function(id) 
+    {
+      openLeft();
+    };
+    interpreter.setProperty(scope, 'openLeft',
+    interpreter.createNativeFunction(wrapper));
+		
+    wrapper = function(id) 
+    {
+      openRear();
+    };
+    interpreter.setProperty(scope, 'openRear',
+    interpreter.createNativeFunction(wrapper));
+		
+    wrapper = function(id) 
+    {
+      openFront();
+    };
+    interpreter.setProperty(scope, 'openFront',
+    interpreter.createNativeFunction(wrapper));
+			
+			// Add an API function for highlighting blocks.
+    var wrapper = function(id) 
+    {
+      id = id ? id.toString() : '';
+      return interpreter.createPrimitive(highlightBlock(id));
+    };
+    interpreter.setProperty(scope, 'highlightBlock',
+    interpreter.createNativeFunction(wrapper));
+  }
+  
+  //function to highlight blocks
+  function highlightBlock(id) 
+  {
+    demoWorkspace.highlightBlock(id);
+    highlightPause = true;
   }
   
   function moveForward() {
@@ -130,10 +226,45 @@ function GameEngine(Debug) {
   //return boolean true if level won
   this.step = function () {
     debug("GameEng.step() called.");
-    setupInterpreter();
-    if (!interpreter.step()) {
+    //setupInterpreter();
+    /*if (!interpreter.step()) {
       //restart interpreter from the beginning
-    }
+    }*/
+    
+    if (!interpreter) {
+        // First statement of this code.
+        // Clear the program output.
+        resetStepUi();
+        interpreter = new Interpreter(latestCode, initApi);
+
+        // And then show generated code in an alert.
+        // In a timeout to allow the outputArea.value to reset first.
+        setTimeout(function() {
+          highlightPause = true;
+          this.step();
+        }, 1);
+        return;
+      }
+      highlightPause = false;
+      do {
+        try {
+          var hasMoreCode = interpreter.step();
+        } finally {
+          if (!hasMoreCode) {
+            // Program complete, no more code to execute.
+            //outputArea.value += '\n\n<< Program complete >>';
+
+            interpreter = null;
+            resetStepUi();
+
+            // Cool down, to discourage accidentally restarting the program.
+
+            return;
+          }
+        }
+        // Keep executing until a highlight statement is reached,
+        // or the code completes or errors.
+      } while (hasMoreCode && !highlightPause);
   }
   
   //return boolean true if level won
