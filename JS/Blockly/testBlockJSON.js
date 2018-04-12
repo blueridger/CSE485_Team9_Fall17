a1bb50b2e78a699716ab415159792b3a0b94b2dc
wwnjj13 <wwnjj13@gmail.com>
2018-04-11 17:35:18 -0700

    blocklyMajorFix

---
+++ JS/Blockly/testBlockJSON.js
@@ -0,0 +1,235 @@
+var ASUmaroon = '#8c1d40'
+var ASUgold = '#ffc627'
+var ROBOmaroon = '#900101'
+var ROBOgold = '#eee70b'
+var gold = '#ffc627'
+var maroon = '#900101'
+
+Blockly.defineBlocksWithJsonArray([{
+  "type": "condition_to_check",
+  "message0": "checktrue",
+  "inputsInline": true,
+  "output": "Boolean",
+  "colour": 0,
+  "tooltip": "",
+  "helpUrl": "",
+  "enableContextMenu" : false
+},
+{
+  "type": "if",
+  "message0": "If %1 Then %2",
+  "enableContextMenu" : false,
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "Condition",
+      "check": "Boolean"
+    },
+    {
+      "type": "input_statement",
+      "name": "Statement",
+      "check": [
+        "right_statement",
+        "back_statement",
+        "left_statement",
+        "inner_if",
+        "forward_statement"
+      ]
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  //"inputsInline": true,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "if_else",
+  "enableContextMenu" : false,
+  "message0": "If %1 Then %2 Else %3",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "Condition",
+      "check": "Boolean"
+    },
+    {
+      "type": "input_statement",
+      "name": "True"
+    },
+    {
+      "type": "input_statement",
+      "name": "False"
+    }
+  ],
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": "",
+  "previousStatement": null,
+  "nextStatement": null
+},
+{
+  "type": "while",
+  "enableContextMenu" : false,
+  "message0": "While %1 Do %2",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "Condition",
+      "check": "Boolean"
+    },
+    {
+      "type": "input_statement",
+      "name": "Statement"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  //"inputsInline": true,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "do_while",
+  "enableContextMenu" : false,
+  "message0": "Do %1 While %2",
+  "args0": [
+    {
+      "type": "input_statement",
+      "name": "Statement",
+      "check": [
+        "right_statement",
+        "back_statement",
+        "left_statement",
+        "inner_if",
+        "forward_statement"
+      ]
+    },
+    {
+      "type": "input_value",
+      "name": "Condition",
+      "check": "Boolean"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  //"inputsInline": true,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "back_statement",
+  "enableContextMenu" : false,
+  "message0": "Go Back",
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": gold,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "forward_statement",
+  "enableContextMenu" : false,
+  "message0": "Go Forward",
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": gold,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "left_statement",
+  "enableContextMenu" : false,
+  "message0": "Turn Left",
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": gold,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "right_statement",
+  "enableContextMenu" : false,
+  "message0": "Turn Right",
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": gold,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "open_left",
+  "enableContextMenu" : false,
+  "message0": "Open Left",
+  "output": null,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "open_right",
+  "enableContextMenu" : false,
+  "message0": "Open Right",
+  "output": null,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "open_front",
+  "enableContextMenu" : false,
+  "message0": "Open Front",
+  "output": null,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "open_back",
+  "enableContextMenu" : false,
+  "message0": "Open Back",
+  "output": null,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "wall_left",
+  "enableContextMenu" : false,
+  "message0": "Wall Left",
+  "output": null,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "wall_right",
+  "enableContextMenu" : false,
+  "message0": "Wall Right",
+  "output": null,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "wall_front",
+  "enableContextMenu" : false,
+  "message0": "Wall Front",
+  "output": null,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "wall_back",
+  "enableContextMenu" : false,
+  "message0": "Wall Back",
+  "output": null,
+  "colour": maroon,
+  "tooltip": "",
+  "helpUrl": ""
+}
+]);
