31d638c0a6a5938f24d85151004f776d910778ce
blueridger <nath.sears@gmail.com>
2018-04-20 16:19:59 -0700

    minor

--- Configurations/ExampleAlternateSettings.js
+++ Configurations/ExampleAlternateSettings.js
@@ -3,7 +3,7 @@ function InitializeGame() {
     debug: true,
     //mapSource expects array of string relative paths to use for map loading, will randomly generate map if null.
     mapSource: ["Maps/TestSet/ExampleMap.json"],
-    numberOfLevels: 3
+    numberOfLevels: 3 //only used if mapSource is null
   }
   blocklyCustomSettings = {
     enableWhile: false,
