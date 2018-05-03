8c648be8a21ad3da274e9e8af68851855ba416da
Edward Borroughs <edward.borroughs@gmail.com>
2018-05-02 21:36:52 -0700

    Map and MapGenerator changes
    The Map class now no longer needs the border walls in the input, just the walls inside the map. Both vertical and horizontal walls are now read left to right, top to bottom.

--- JS/MapGenerator.js
+++ JS/MapGenerator.js
@@ -67,32 +67,28 @@ function GenerateMap(width, height) {
 		tileSet[i] = new Tile();
 		MakeSet(tileSet[i]);
 	}
-	var numWalls = width + (2 * width + 1) * height;
+	var numWalls = height * (width - 1) + width * (height - 1);
 	var wallArray = new Array(numWalls);
 	for(i = 0; i < numWalls; i++) {
 		wallArray[i] = i;
 	}
-	var randomVWalls = new Array(height * (width + 1));
+	var randomVWalls = new Array(height * (width - 1));
 	for(i = 0; i < randomVWalls.length; i++) {
 		randomVWalls[i] = true;
 	}
-	var randomHWalls = new Array(width * (height + 1));
+	var randomHWalls = new Array(width * (height - 1));
 	for(i = 0; i < randomHWalls.length; i++) {
 		randomHWalls[i] = true;
 	}
 	for(i = numWalls; i > 0; i--) {
 		wallIndex = wallArray.splice(Math.floor((Math.random() * i)), 1);
-		if(wallIndex % (2 * width + 1) >= width) { //Check if wall is vertical
-			if((wallIndex % (2 * width + 1)) % width !== 0) { //Check if wall is not a border. Do nothing if it is.
-				leftTileIndex = wallIndex - (width + 1 + parseInt(wallIndex / (2 * width + 1), 10) * (width + 1));
-				randomVWalls[wallIndex - width * (parseInt(wallIndex / (2 * width + 1), 10) + 1)] = !Union(tileSet[leftTileIndex], tileSet[leftTileIndex + 1]);
-			}
+		if(wallIndex % (2 * width - 1) < width - 1) { //Check if wall is vertical
+			leftTileIndex = wallIndex - (width - 1) * parseInt(wallIndex / (2 * width - 1), 10);
+			randomVWalls[wallIndex - width * parseInt(wallIndex / (2 * width - 1), 10)] = !Union(tileSet[leftTileIndex], tileSet[leftTileIndex + 1]);
 		}
 		else { //wall is horizontal
-			if(wallIndex >= width && wallIndex < (numWalls - width)) { //Check if wall is not a border. Do nothing if it is.
-				topTileIndex = wallIndex - (2 * width + 1 + (parseInt(wallIndex / (2 * width + 1), 10) - 1) * (width + 1));
-				randomHWalls[topTileIndex + height * (topTileIndex % width) - (width - 1) * parseInt(topTileIndex / width, 10) + 1] = !Union(tileSet[topTileIndex], tileSet[topTileIndex + width]);
-			}
+			topTileIndex = wallIndex - (parseInt(wallIndex / (2 * width - 1), 10) + 1) * (width - 1);
+			randomHWalls[topTileIndex] = !Union(tileSet[topTileIndex], tileSet[topTileIndex + width]);
 		}
 	}
 	var randomMap = new Map(width, height, randomVWalls, randomHWalls);
